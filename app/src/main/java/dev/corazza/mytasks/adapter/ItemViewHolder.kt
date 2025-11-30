fe784d641ff777db4ff00c77a88d1024249408d0
Marcos <mcs.corazza@gmail.com>
2025-11-30 16:50:24 -0300

    req 8: Customization of cards layout

--- app/src/main/java/dev/corazza/mytasks/adapter/ItemViewHolder.kt
+++ app/src/main/java/dev/corazza/mytasks/adapter/ItemViewHolder.kt
@@ -1,5 +1,6 @@
 package dev.corazza.mytasks.adapter
 
+import android.view.View
 import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.RecyclerView
 import dev.corazza.mytasks.R
@@ -17,6 +18,7 @@ class ItemViewHolder(
 
   fun setData(task : Task) {
     binding.tvTitle.text = task.title
+    binding.tvDescription.text = task.description
 
     val today = LocalDate.now()
 
@@ -27,26 +29,31 @@ class ItemViewHolder(
       task.date.isEqual(today) -> R.color.yellow
       else -> R.color.blue
     }
-    binding.tvTitle.setBackgroundResource(colorResource)
+    binding.viewStatusColor.setBackgroundResource(colorResource)
 
     val context = binding.root.context
     val prefs = PreferenceManager.getDefaultSharedPreferences(context)
     val isFullFormat = prefs.getBoolean("date_format", false)
 
     if (task.date != null) {
-      val pattern = if (isFullFormat) {
-        "dd 'de' MMMM 'de' yyyy"
-      } else {
-        "dd/MM/yyyy"
-      }
       val locale = Locale.forLanguageTag("pt-BR")
+      val pattern = if (isFullFormat) "dd 'de' MMMM 'de' yyyy" else "dd/MM/yyyy"
       val formatter = DateTimeFormatter.ofPattern(pattern, locale)
-      val dateStr = task.date.format(formatter)
-      val timeStr = task.time?.let { "$it" } ?: ""
-      binding.tvDate.text = "$dateStr | $timeStr"
+      binding.tvDate.text = task.date.format(formatter)
+
+      if (task.time != null) {
+        binding.tvTime.text = task.time.toString()
+        binding.tvTime.visibility = View.VISIBLE
+      } else {
+        binding.tvTime.text = ""
+        binding.tvTime.visibility = View.INVISIBLE
+      }
     } else {
-      binding.tvDate.text = ""
+      binding.tvDate.setText(R.string.no_date)
+      binding.tvTime.visibility = View.INVISIBLE
+
     }
+
     binding.root.setOnClickListener {
       listener.onClick(task)
     }
